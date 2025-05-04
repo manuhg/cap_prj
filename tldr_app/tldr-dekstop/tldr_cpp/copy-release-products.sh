8c021b6be06e0d3fa4f7709831cecd9936f1d0bb
Manu Hegde <manuhegdev@gmail.com>
2025-05-03 18:45:27 -0700

    some re organization in release products dir

---
+++ tldr_app/tldr-dekstop/tldr_cpp/copy-release-products.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ln -s /opt/homebrew/opt/libpq/lib/ /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/libpq
+ln -s /opt/homebrew/opt/libpqxx/lib/ /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/libpqxx
+ln -s /opt/homebrew/opt/poppler/lib/ /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/poppler
+ln -s /opt/homebrew/opt/openssl/lib /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/openssl
+ln -s /opt/homebrew/opt/sqlite/lib /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/sqlite
+ln -s /opt/homebrew/opt/icu4c/lib /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/icu4c
+ln -s /opt/homebrew/opt/zlib/lib /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/zlib
+ln -s /opt/homebrew/opt/curl/lib /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/system/curl
+
+
+cp -v libtldr.a /Users/manu/proj_tldr/tldr-dekstop/release-products/libs/
+cp -v ../src/lib_tldr/tldr_api.h /Users/manu/proj_tldr/tldr-dekstop/release-products/include/
