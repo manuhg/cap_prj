232ad322567b52a26e963eacf4f055f30227d816
Manu Hegde <manuhegdev@gmail.com>
2025-05-14 19:06:32 -0700

    theoritical background WIP. done with LLM & its inference components

--- docs/reports/final_report/report/show_dup_citations.sh
+++ docs/reports/final_report/report/show_dup_citations.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-pbpaste| grep -Eo '@[^$]+' |sort|uniq -c|sort | grep -v '1 @'
+#pbpaste| grep -Eo '@[^$]+' |sort|uniq -c|sort | grep -v '1 @'
+pbpaste| grep -Eo '@[^{]+([^,]+)'|grep -Eo '{[^$]+' |sort|uniq -c|sort | grep -v '1 {'
